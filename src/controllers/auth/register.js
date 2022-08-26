59ad8eeefb9d23429e2b0f1db216916711b76797
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-08-25 22:52:29 -0300

    Added user register controller

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -1,7 +1,35 @@
-const register = (req, res) => {
-	res.json({
-		msg: "register"
-	});
+const { connect, disconnect } = require("../../config/dbConnection");
+const { encryptPassword } = require("../../utils/encryptPassword");
+
+const register = async (req, res) => {
+	const pool = await connect();
+
+	const { username, email, password } = req.body;
+
+	const encryptedPass = await encryptPassword(password);
+
+	try {
+		const queryResponse = await pool.query('INSERT INTO schemamovies.users(username, email, password) VALUES($1, $2, $3) RETURNING "idUsers", "username", "email"', [username, email, encryptedPass]);
+
+		const newUser = queryResponse.rows;
+
+		res.json({
+			data: newUser,
+			success: true,
+			message: "user registered successfully.",
+			error: null
+		});
+	} catch (err) {
+		console.error(err);
+		res.json({
+			data: null,
+			success: false,
+			message: "something went wrong",
+			error: err
+		});
+	} finally {
+		disconnect(pool);
+	}
 };
 
 module.exports = register;
